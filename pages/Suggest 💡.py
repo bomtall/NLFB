ba8f2dcf93ac77a39c3852a4066f50d9ac6104b4
Thomas <=>
2024-09-08 13:13:04 +0100

    add suggestion form

---
+++ pages/Suggest 💡.py
@@ -0,0 +1,68 @@
+import toml
+import json
+import millify
+import gspread
+import requests
+import calendar
+import numpy as np
+import pandas as pd
+import polars as pl
+import datetime as dt
+import streamlit as st
+from millify import prettify
+from lxml.html import fromstring
+from streamlit_gsheets import GSheetsConnection
+from google.oauth2.service_account import Credentials
+from oauth2client.service_account import ServiceAccountCredentials
+
+import utils
+
+st.set_page_config(
+    page_title="Suggest a book",
+    page_icon="📚",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+ENV = toml.load('.streamlit/secrets.toml')
+WORKBOOK = utils.authenticate(
+    ENV['connections']['gsheets'],
+    ENV['scopes']['scope'], 
+    'NLFB'
+)
+
+def add_suggestion(data):
+    sh = WORKBOOK.worksheet('Suggestions') 
+    sh.append_row(data)
+
+with st.form("my_form"):
+
+    st.write("Use this form to suggest a future booklub pick for London's Friendly Bookclub...")
+    st.write("If you want to be contacted, notified or credited, if your suggestion is chosen, please fill out your details")
+    user_name = st.text_input("Your name...")
+    user_email = st.text_input("Your email")
+    st.write("Your suggestion")
+    book_title = st.text_input("Suggested book title...")
+    author = st.text_input("Author")
+    url = st.text_input("Link to the book...")
+    user_rating = st.slider("Have you read the book? If so, what did you rate it?", min_value=0.0, max_value=10.0, step=0.1)
+    why_suggest = st.text_area("Tell us why we should make it a bookclub pick...")
+    submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        data = [
+            book_title,
+            author,
+            url,
+            user_rating,
+            why_suggest,
+            dt.datetime.now().strftime('%d/%m/%Y %H:%M:%S'),
+            user_name,
+            user_email
+        ]
+        add_suggestion(data)
+        st.write("Thank you for your suggestion of: " + book_title)
+        
+        
+
+
---
+++ utils.py
@@ -0,0 +1,41 @@
+import toml
+import json
+import millify
+import gspread
+import requests
+import calendar
+import numpy as np
+import pandas as pd
+import polars as pl
+import datetime as dt
+import streamlit as st
+from millify import prettify
+from lxml.html import fromstring
+from streamlit_gsheets import GSheetsConnection
+from google.oauth2.service_account import Credentials
+from oauth2client.service_account import ServiceAccountCredentials
+
+def authenticate(connection_values: dict, scope: list, workbook_name: str):
+    credentials_file = json.loads(str(connection_values).replace("'", '"').replace('\r\n', '\\r\\n'))
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_file, scopes=scope)
+    client = gspread.authorize(credentials)
+    wb = client.open(workbook_name)
+    return wb
+
+def pad_data(data: list, length: int):
+    padded_data = [
+        [None if x == "" else x for x in row] +
+        [None] * (length - len(row)) for row in data
+    ]
+    # for i in range(len(data)):
+        # data[i] = [None if x == "" else x for x in data[i]]
+        # data[i] = [row + [None] * (length - len(row)) for row in data]
+    return padded_data
+
+def load_data(sheet_name: str, schema: dict, wb: any) -> pl.DataFrame:
+    sheet = wb.worksheet(sheet_name)
+    data = sheet.get()
+    headers = data[0]
+    padded_data = pad_data(data[1:], len(headers))
+    loaded_dataframe = pl.DataFrame(padded_data, schema=schema, orient='row', strict=False)
+    return loaded_dataframe
